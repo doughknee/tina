11088c5d6aabf5d05e0e45a290ccbd166954ca24
Brandon Harris <brandon@relentnet.com>
2026-09-01 03:40:48 -0500

    Bump version to 1.2.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -74,8 +74,8 @@ android {
         applicationId = "com.tina.app"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
     }
 
     signingConfigs {
@@ -133,7 +133,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Msi)
             packageName = "tina"
-            packageVersion = "1.1.0"
+            packageVersion = "1.2.0"
         }
     }
 }
